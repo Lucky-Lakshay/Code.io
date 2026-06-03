a677177a699e78a0f4f7f3bfdda7bd8b6005a5ae
Lucky-Lakshay <luckatmyside99@gmail.com>
2026-06-03 14:22:06 +0530

    home page username(only alphabets), clicking enter or join room takes us to workplace

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+    const [username, setUsername] = useState("");
+    const [hasJoined, setHasJoined] = useState(false);
+
+    const handleJoin = () => {
+    if (username.trim() !== "") {
+        setHasJoined(true);
+    }
+    };
+    
+  return (
+    <>
+        {hasJoined?(
+        <div>
+            <h1>Welcome to the Workspace, {username}!</h1>
+            <p>The code editor will go here.</p>
+        </div>
+        )
+        :(
+        <div className="flex flex-col min-h-screen items-center justify-center">
+            <h1 className="text-7xl mb-7">
+                Welcome to Code.io
+            </h1>
+            <input
+            className="placeholder:text-center text-center px-4 py-2 border border-neutral-700 rounded bg-neutral-800 focus:outline-none focus:border-blue-500 mb-4"
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e)=>{
+                const value = e.target.value;
+                if (/^[A-Za-z]*$/.test(value)) {
+                setUsername(value);}
+            }}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                    handleJoin();
+            }}}
+            />
+            <button onClick={handleJoin} className="px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded transition-colors">Join Room</button>
+        </div>
+        )}
+  </>);
+}
