1652cc3b82c8501dd368476268d2fd557313a9f8
Lucky-Lakshay <luckatmyside99@gmail.com>
2026-06-05 20:58:56 +0530

    set up monaco editor, with dif lang opt and run on nav

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useState } from "react";
 import Navbar from './components/Navbar'; 
+import Editor from "@monaco-editor/react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 export default function Home() {
     const [username, setUsername] = useState("");
     const [hasJoined, setHasJoined] = useState(false);
+    const [language, setLanguage] = useState("c");
 
     const handleJoin = () => {
     if (username.trim() !== "") {
@@ -23,7 +25,33 @@ export default function Home() {
             <PanelGroup direction="horizontal">
             
             <Panel defaultSize={50} minSize={20}>
-                input
+                <div className="flex flex-col h-full">
+                    <div className="bg-neutral-800 p-2 flex justify-between items-center border-b border-neutral-700">
+                    
+                    <select 
+                    value={language} 
+                    onChange={(e) => setLanguage(e.target.value)}
+                    className="bg-neutral-700 text-white px-2 py-1 rounded text-sm outline-none"
+                    >
+                    <option value="c">C</option>
+                    <option value="cpp">C++</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="python">Python</option>
+                    <option value="java">Java</option>
+                    </select>
+                    <button className="bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded text-sm transition-colors ml-auto">
+                        Run Code
+                    </button>
+                    </div>
+                    <div className="flex-grow">
+                    <Editor
+                        height="100%"
+                        language={language}
+                        theme="vs-dark"
+                    />
+                    </div>
+
+                </div>
             </Panel>
 
             <PanelResizeHandle className="w-2 bg-neutral-700 cursor-col-resize" />
